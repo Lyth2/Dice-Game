2598d25b606578626ab8a81afa1dff6ad1de4d3d
Lily Lyth <78177091+Lyth2@users.noreply.github.com>
2021-07-28 02:23:08 +0000

    put in the do loop however it failed so I will need to fix it

--- index.js
+++ index.js
@@ -3,32 +3,46 @@
 //27/07/2021 
 
 
-console.log('pls roll the dice')
+
 
 let point1 = 0;
 let point2 = 0;
 
+do{
+
+console.log('pls roll the dice, first to 20 wins')
 
 let player1Number = prompt('Player 1 what number did you get')
 let player2Number = prompt('Player 2 what number did you get')
 
-while (player1Number || player2Number >= 7){
-  console.log('sorry that is invalid pls put in a smaller number')
-  
+while (player1Number >= 7){
+    console.log('Sorry player1 number is invalid.');
+    player1Number = prompt('Player 1 what number did you get?');
 }
 
+while (player2Number >= 7){
+    console.log('Sorry player2 number is invalid.');
+    player2Number = prompt('Player 2 what number did you get?');
+}
 
 if (player1Number == player2Number) {
   console.log('you rolled the same number, you both get a point!')
-  point1 ++
-  point2 ++
+ point1 ++
+ point2 ++
+ console.log('Player 1 you have ' + point1 + ' points')
+ console.log('Player 2 you have ' + point2 + ' points')
  
 } else if (player1Number < player2Number){ 
   console.log('player 2, your number is bigger you get a point')
   point2 ++
+  console.log('Player 2 you have ' + point2 + ' points')
+  
 } else if (player1Number > player2Number){
   console.log('player 1 your number is bigger, you get a point')
   point1 ++
+  console.log('Player 1 you have ' + point1 + ' points') 
+  
+}
 }
 
- 
+
